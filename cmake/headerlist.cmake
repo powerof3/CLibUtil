ae6b7fde091baf106c9394cb219db81b3827f3a8
powerof3 <32599957+powerof3@users.noreply.github.com>
2022-11-07 15:45:03 +0530

    Renamed `config` to `distribution`, added common distribution functions for getting record type

--- cmake/headerlist.cmake
+++ cmake/headerlist.cmake
@@ -1,7 +1,8 @@
 set(headers ${headers}
-	include/CLIBUtil/config.hpp
 	include/CLIBUtil/detail/XoshiroCpp.hpp
+	include/CLIBUtil/distribution.hpp
 	include/CLIBUtil/numeric.hpp
 	include/CLIBUtil/rng.hpp
 	include/CLIBUtil/string.hpp
+	include/CLIBUtil/utils.hpp
 )
---
+++ include/CLIBUtil/utils.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "distribution.hpp"
+#include "numeric.hpp"
+#include "rng.hpp"
+#include "string.hpp"
