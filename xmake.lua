0b481d3029bfcda8b02da9714a9014b630d0fa71
Styyx <styyx228@gmail.com>
2026-08-24 11:07:37 +0200

    added Invalid Key return to avoid crash

--- xmake.lua
+++ xmake.lua
@@ -1,4 +1,8 @@
+set_encodings("utf-8") -- msvc: /utf-8
+set_encodings("source:utf-8", "target:utf-8")
+
 target("clib-util")
     set_kind("headeronly")
     add_headerfiles("include/**.h", "include/**.hpp")
     add_includedirs("include", {public = true})
+    
